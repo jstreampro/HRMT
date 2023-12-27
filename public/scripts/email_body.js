35cfa1109d68004288c4876f4a741ef9638ef726
Kfir Gershon <kfir@sys-tech.co.il>
2023-12-27 12:18:14 +0200

    update email body

--- public/scripts/email_body.js
+++ public/scripts/email_body.js
@@ -1,14 +1,15 @@
 export const EMAIL_BODY =`We are writing to convey the deep sense of betrayal felt by us, Israeli and Jewish women around the world, due to your actions—or rather, your lack of action. Jewish women worldwide, feel betrayed by your disregard to the atrocities committed against Jewish women by Hamas, including sexual abuse, rape, amputation, burning alive, the abduction and murder of women, adolescents, and girls on October 7th. 
 
-The #Hamas_Raped_MeToo movement  is demanding a public condemnation, appearing on your social media platforms and on a visible place on your website, of these horrific acts of gender-based violence by December 20th. Failure to do so will result in your organization being listed on the highly visible 'Wall of Shame' (www.hamasrapedmetoo.com). We urge your organization to speak up against these crimes to reaffirm the rights and safety of women. 
+The #Hamas_Raped_MeToo movement is demanding a public condemnation, appearing on your social media platforms and on a visible place on your website, of these horrific acts of gender-based violence. Failure to do so will result in your organization being listed on the highly visible 'Wall of Shame' (www.hamasrapedmetoo.com). We urge your organization to speak up against these crimes to reaffirm the rights and safety of women. 
 
 Your organization has no reason to exist if it remains silent in the face of these atrocities: the apparent rape of Shani Louk, as depicted in a Hamas video, where her naked body is callously thrown into a pickup truck as if she were mere prey; the video showing a young girl, being dragged by her hair into a car, with her crotch area stained with blood; the heart-wrenching cries of a mother being raped by terrorists one after another, while her baby is tragically baked in an oven. And these are just a few among the growing number of horrific testimonies being revealed as the days pass. These were not random acts of violence, but rather actions carried out as per specific written instructions from the terrorist leaders.
 
 The eyewitness reports we’ve seen are too hard to read. One report describes a woman is repeatedly raped by one terrorist after another. Then, the last one mutilates her by chopping her breasts off before murdering her!!
 
+
 Considering this, we demand:
 
-1.	Public Condemnation: A strong, unequivocal condemnation prominently displayed on your social media platforms and website by December 20th. This statement must reflect a commitment to justice and human rights.
+1.	Public Condemnation: A strong, unequivocal condemnation prominently displayed on your social media platforms and website. This statement must reflect a commitment to justice and human rights.
 
 2.	We urgently call for your organization to take active measures for the release of remaining hostages who endure threats and sexual harassment.
 
@@ -16,15 +17,13 @@ Considering this, we demand:
 
 4.	Visibility on 'Wall of Shame': Failure to fully condemn these acts will result in your organization being included on our 'Wall of Shame' at www.hamasrapedmetoo.com. We urge your organization to speak out against these crimes to safeguard the rights and safety of women.
 
-5.	Change from Black to Pink: Upon issuing a bold public statement of condemnation, we commit to symbolically change your logo from black to pink on our site, signifying a steadfast commitment to combating gender-based violence.
-
 We will not let you continue deceiving women worldwide into believing you care about them. We will take whatever action necessary to hold you and your organization accountable.
 
-Break this disgraceful silence at once and undo the shame you have brought upon yourselves. Please send us your public statement of commitment or condemnation as soon as possible, and no later than December 20th. 
+Break this disgraceful silence at once and undo the shame you have brought upon yourselves. Please send us your public statement of commitment or condemnation as soon as possible. 
 
 No woman can stay silent! If you don't speak up, it's as if you allow Hamas to rape MeToo!
 
-Hamas Raped MeToo movement.
+ Hamas Raped MeToo movement.
 
 Send us your condemnation to: 
 contact@hamasrapedmetoo.info`;
